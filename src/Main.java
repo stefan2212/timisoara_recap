fd6b8baeb4334f478cca8606e9266e96af9c2668
stefan2212 <stefan.cernescu94@gmail.com>
2020-06-27 12:19:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import factory.PastaFactory;
 public class Main {
     public static void main(String[] args) {
         PastaFactory pastaFactory = new PastaFactory();
-        Pasta pasta = pastaFactory.getPasta("Carbonara");
+        Pasta pasta = pastaFactory.getPasta("Carbonara1");
         Pasta pasta2 = pastaFactory.getPasta("Bolognese");
         System.out.println(pasta.getDetectedPasta());
         System.out.println(pasta2.getDetectedPasta());
