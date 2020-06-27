062c873eba1cced64bb646350bc33fc74f2893dc
stefancernescu <stefan.cernescu94@gmail.com>
2020-06-27 11:28:02 +0300

    some wrong commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import factory.PastaFactory;
 public class Main {
     public static void main(String[] args) {
         PastaFactory pastaFactory = new PastaFactory();
-        Pasta pasta = pastaFactory.getPasta("Carbonara");
+        Pasta pasta = pastaFactory.getPastadee("osdnfosdnfds");
         System.out.println(pasta.getDetectedPasta());
     }
 }
